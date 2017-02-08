305c9bf4b274c2f601e38408eb74d5164dbdf4f2
R <rafal.paprocki@gmail.com>
2017-02-08 19:29:13 +0700

    IQ and Rest1 blinks FP1 and FP2 imported; FP1 for IQ and for Rest1-missed blinks were add

--- rav.m
+++ rav.m
@@ -6,24 +6,24 @@ clc
 
 
 %%
-listing=dir('DATA/*.TXT');
+listing=dir('DATA/Rest1/*.TXT');
 number_of_files = length(listing);
 
 
 
 for i=1:number_of_files %WHY not PARFOR?
-    eeg = loadeeg(['DATA/',listing(i).name], [1]); %1=fp1, 2=fp2
+    eeg = loadeeg(['DATA/Rest1/',listing(i).name], [2]); %1=fp1, 2=fp2
     len = length(eeg)/(250*60);
-    if(len>11) %shorter than 10 minuts needs to be exported again
+    if(len<5) %shorter than 10 minuts needs to be exported again
     disp([int2str(i),' ',listing(i).name,' ',num2str(len)])  
     end
     if(i==8 || i==18) %Eric and Sepi were sampled with 500Hz instead of 250
       eeg = eeg(1:2:end); %resampling
     end
     size(eeg)
-    dataFP1(i,:) = eeg(1:150000);
+    dataFP1(i,:) = eeg(1:75000); %75000 for resting, 150000 for IQ
 
 end
 
 
-save FP1.mat dataFP1
+save FP1-R2.mat dataFP1
